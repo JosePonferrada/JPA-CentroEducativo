690334747b4bdd1570ffae14be43c461e9d4041d
JosePonferrada <joseponfe10@gmail.com>
2024-04-11 14:17:56 +0200

    feat(JPA): first commit of files with entities and main

---
+++ src/main/java/ejercicio15_CentroEducativo/entities/Materia.java
@@ -0,0 +1,55 @@
+package ejercicio15_CentroEducativo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "materia")
+public class Materia {
+
+	@Id
+	private int id;
+	private int cursoId;
+	private String nombre;
+	private String acronimo;
+	
+	public Materia() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCursoId() {
+		return cursoId;
+	}
+
+	public void setCursoId(int cursoId) {
+		this.cursoId = cursoId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAcronimo() {
+		return acronimo;
+	}
+
+	public void setAcronimo(String acronimo) {
+		this.acronimo = acronimo;
+	}
+	
+}
---
+++ src/main/java/ejercicio15_CentroEducativo/entities/ValoracionMateria.java
@@ -0,0 +1,65 @@
+package ejercicio15_CentroEducativo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "valoracionmateria")
+public class ValoracionMateria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private int idProfesor;
+	private int idEstudiante;
+	private int idMateria;
+	private float valoracion;
+	
+	public ValoracionMateria() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdProfesor() {
+		return idProfesor;
+	}
+
+	public void setIdProfesor(int idProfesor) {
+		this.idProfesor = idProfesor;
+	}
+
+	public int getIdEstudiante() {
+		return idEstudiante;
+	}
+
+	public void setIdEstudiante(int idEstudiante) {
+		this.idEstudiante = idEstudiante;
+	}
+
+	public int getIdMateria() {
+		return idMateria;
+	}
+
+	public void setIdMateria(int idMateria) {
+		this.idMateria = idMateria;
+	}
+
+	public float getValoracion() {
+		return valoracion;
+	}
+
+	public void setValoracion(float valoracion) {
+		this.valoracion = valoracion;
+	}
+	
+}
